dfcac29cc56a3b77ed5ec50815462b6a30492bb6
Horev Vladimir <vshorev@yandex.ru>
2025-06-07 23:34:09 +0700

    1

--- components/uplink.vue
+++ components/uplink.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="fixed rounded-full bg-blue-300 fixed-block transition-all duration-500 hover:scale-110 ">
-        <a href="#part">
-            <img width="30" height="30" class=" mx-auto mt-2" src="https://img.icons8.com/ios/50/chevron-up.png" alt="chevron-up" />
-        </a>
-    </div>
+    <a href="#part">
+        <div class="fixed rounded-full bg-blue-300 fixed-block transition-all duration-500 hover:scale-110 ">
+            <img width="30" height="30" class=" mx-auto mt-2" src="https://img.icons8.com/ios/50/chevron-up.png"
+                alt="chevron-up" />
+        </div>
+    </a>
 </template>
 
 <style>
@@ -16,5 +17,5 @@
     width: 50px;
     height: 50px;
     z-index: 500;
-} 
+}
 </style>
